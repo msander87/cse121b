6fe2a2390f37ad8d9a4f1a447a1ca99404b9ef9b
msander87 <msander1987@gmail.com>
2021-10-23 01:07:04 -0300

    alert

--- js/scripts.js
+++ js/scripts.js
@@ -122,7 +122,7 @@ document.querySelector("#search").addEventListener("click", function () {
         document.querySelector(".instructions").innerHTML = ""
     
         const urlBOM = `https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang=${lang}&uri=/scriptures/bofm/${book}/${chapter}/1`
-        let results = null;
+        let results = [];
     
         // Should reset the languages in the HTML, otherwise the chosen order can't be achieved
         resetLanguageHTML(selectedLanguage1, selectedLanguage2);
@@ -139,10 +139,10 @@ document.querySelector("#search").addEventListener("click", function () {
             // console.log(data);
             const outputBOMElement = document.querySelector(`#${lang}`);
             results = data
-            const chapterBody = results.content.body;
+            //const chapterBody = results.content.body;
             // Somewhere over here replace("/study/","https://www.churchofjesuschrist.org/study/") so the footnotes actually work
-            outputBOMElement.innerHTML = chapterBody;
-    
+            //outputBOMElement.innerHTML = chapterBody;
+            alert(results);
             // const footer = document.querySelector("footer");
             // footer.innerHTML = "";
         }
