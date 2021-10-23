4b05edcf3169927d56e522f3177c43e4e78d1aba
msander87 <msander1987@gmail.com>
2021-10-23 01:40:49 -0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
 
+const templeList = [];
 
-  const urlBOM = 'https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang=eng&uri=/scriptures/bofm/jacob/1'
-  let results = null;
 
-  fetch(urlBOM)
+
+const output = (temples) => {
+  temples.forEach(
+      temple => {
+          let article = document.createElement('article');
+
+          let templeName = document.createElement('h3');
+          templeName.textContent = temple.book_title;
+
+          let location = document.createElement('h4');
+          location.textContent = temple.chapter_number;
+
+          let dedicated = document.createElement('p');
+          dedicated.textContent = temple.scripture_text;
+
+          
+
+          article.appendChild(templeName);
+          article.appendChild(location);
+          article.appendChild(dedicated);
+          
+
+          document.querySelector('#con').appendChild(article);
+      }
+  );
+}
+
+
+fetch('https://msander87.github.io/cse121b/json/bom.json')
     .then(response => response.json())
     .then(temples => {
-        results = temples;
-        
-  });
-
-
-alert(results);
+        templeList = temples;
+        output(templeList);
+    });
--- js/task5.js
+++ js/task5.js
@@ -89,22 +89,20 @@ const output = (temples) => {
             let article = document.createElement('article');
 
             let templeName = document.createElement('h3');
-            templeName.textContent = temple.templeName;
+            templeName.textContent = temple.book_title;
 
             let location = document.createElement('h4');
-            location.textContent = temple.location;
+            location.textContent = temple.chapter_number;
 
-            let dedicated = document.createElement('h4');
-            dedicated.textContent = temple.dedicated;
+            let dedicated = document.createElement('p');
+            dedicated.textContent = temple.scripture_text;
 
-            let img = document.createElement('img');
-            img.setAttribute('src', temple.imageUrl);
-            img.setAttribute('alt', temple.templeName);
+            
 
             article.appendChild(templeName);
             article.appendChild(location);
             article.appendChild(dedicated);
-            article.appendChild(img);
+            
 
             document.querySelector('#temples').appendChild(article);
         }
