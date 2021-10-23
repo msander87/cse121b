a7c31a6f55dacff55457a821d1592bc137983d60
msander87 <msander1987@gmail.com>
2021-10-23 00:44:03 -0300

    changes

--- js/scripts copy.js
+++ js/scripts copy.js
@@ -116,17 +116,17 @@ selLang2.addEventListener("change", function () {
 });
 
 document.querySelector("#search").addEventListener("click", function () {
-    function fetchDataIn(book = "Nach", chapter, lang) {
+    function fetchDataIn(book, chapter, lang) {
 
         // Delete the intro after the first search
         document.querySelector(".instructions").innerHTML = ""
-
+    
         const urlBOM = `https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang=${lang}&uri=/scriptures/bofm/${book}/${chapter}`
         let results = null;
-
+    
         // Should reset the languages in the HTML, otherwise the chosen order can't be achieved
         resetLanguageHTML(selectedLanguage1, selectedLanguage2);
-
+    
         function convertToJson(response) {
             if (response.ok) {
                 return response.json();
@@ -134,7 +134,7 @@ document.querySelector("#search").addEventListener("click", function () {
                 console.log("error:", response);
             }
         }
-
+    
         function displayScripts(data) {
             // console.log(data);
             const outputBOMElement = document.querySelector(`#${lang}`);
@@ -142,16 +142,16 @@ document.querySelector("#search").addEventListener("click", function () {
             const chapterBody = results.content.body;
             // Somewhere over here replace("/study/","https://www.churchofjesuschrist.org/study/") so the footnotes actually work
             outputBOMElement.innerHTML = chapterBody;
-
+    
             // const footer = document.querySelector("footer");
             // footer.innerHTML = "";
         }
-
+    
         fetch(urlBOM).then(convertToJson).then(displayScripts);
     }
-
-    fetchDataIn(selectedBook, selectedChapter, selectedLanguage1)
-    fetchDataIn(selectedBook, selectedChapter, selectedLanguage2)
+    
+    fetchDataIn("jacob", 1, "eng")
+    fetchDataIn("jacob", 1, "por")
 });
 
 resetLanguageHTML(selectedLanguage1, selectedLanguage2);
