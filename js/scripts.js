c5d1721d684a3e9e061de2abf83585e8fe979658
msander87 <msander1987@gmail.com>
2021-10-23 00:48:14 -0300

    chang

--- js/scripts.js
+++ js/scripts.js
@@ -116,12 +116,12 @@ selLang2.addEventListener("change", function () {
 });
 
 document.querySelector("#search").addEventListener("click", function () {
-    function fetchDataIn(book, chapter, lang) {
+    function fetchDataIn() {
 
         // Delete the intro after the first search
         document.querySelector(".instructions").innerHTML = ""
     
-        const urlBOM = `https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang=${lang}&uri=/scriptures/bofm/${book}/${chapter}/1`
+        const urlBOM = `https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang=eng&uri=/scriptures/bofm/jacob/1/1`
         let results = null;
     
         // Should reset the languages in the HTML, otherwise the chosen order can't be achieved
@@ -150,8 +150,8 @@ document.querySelector("#search").addEventListener("click", function () {
         fetch(urlBOM).then(convertToJson).then(displayScripts);
     }
     
-    fetchDataIn("jacob", 1, "eng")
-    fetchDataIn("jacob", 1, "por")
+    fetchDataIn()
+    
 });
 
 resetLanguageHTML(selectedLanguage1, selectedLanguage2);
