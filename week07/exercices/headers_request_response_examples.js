91c55329d4092ac41afbac89b835a193cc3548b3
msander87 <msander1987@gmail.com>
2022-02-18 14:05:48 -0300

    finalizing radings week 7

---
+++ week07/exercices/headers_request_response_examples.js
@@ -0,0 +1,18 @@
+const url = 'https:example.com/data';
+const headers = new Headers({
+    'Content-Type': 'text/plain',
+    'Accept-Charset': 'utf-8',
+    'Accept-Encoding': 'gzip,deflate'
+})
+const request = (url, {
+    headers: headers
+})
+fetch(request)
+    .then(function (response) {
+        if (response.ok) {
+            return response;
+        }
+        throw Error(response.statusText);
+    })
+    /*.then(response =>  do something with response )
+    .catch(error => console.log('There was an error!'))*/
