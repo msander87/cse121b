262bfa2085b6c73e46e62355afcc94599a076b6e
msander87 <msander1987@gmail.com>
2022-03-02 19:33:09 -0300

    finishing ch14

---
+++ week09/exercices/websocket/main.js
@@ -0,0 +1,30 @@
+const URL = 'wss://echo.websocket.org/';
+const outputDiv = document.getElementById('output');
+const form = document.forms[0];
+const connection = new WebSocket('wss://echo.websocket.org/');
+
+
+function output(message) {
+    const para = document.createElement('p');
+    para.innerHTML = message;
+    outputDiv.appendChild(para);
+}
+
+form.addEventListener('submit', message, false);
+
+function message(event) {
+    event.preventDefault();
+    const text = form.message.value;
+    output(`SENT: ${text}`);
+    connection.send(text);
+
+   
+}
+
+connection.addEventListener('message', (event) => {
+    output(`RESPONSE: ${event.data}`);
+}, false);
+
+connection.addEventListener('open', () => {
+    output('CONNECTED');
+}, false);
