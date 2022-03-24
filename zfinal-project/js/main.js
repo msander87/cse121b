8d7b2ff2679e0a7a88fb12358c3085cc3c5ccac7
msander87 <msander1987@gmail.com>
2022-03-24 20:16:26 -0300

    completing views

---
+++ zfinal-project/js/main.js
@@ -0,0 +1,4 @@
+document.querySelector("#login-container").style.display = "none";
+document.querySelector("#instructions-container").style.display = "none";
+document.querySelector("#scores-container").style.display = "none";
+document.querySelector("#home-subheader").style.display = "none";
